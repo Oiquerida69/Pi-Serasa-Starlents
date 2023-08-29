063599882a3639a6c9ccee2efc437c8fca8e1573
DANIELE APARECIDA DOS SANTOS <daniele.asantos32@senacsp.edu.br>
2023-08-29 17:07:34 -0300

    classesss

--- Pi-Serasa-Starlents/Properties/notificacoes.cs
+++ Pi-Serasa-Starlents/Properties/notificacoes.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 
-public class notificacoes
+
+
+namespace Pi_Serasa_Starlents
 {
-	public notificacoes()
-	{
-	}
+    internal class notificacoes
+    {
+        public int id;
+        public string id_usuario;
+        public string conteudo;
+      
+
+
+        public notificacoes(int id, string id_usuario, string conteudo)
+        { 
+            this.id = id;
+            this.id_usuario = id_usuario;
+            this.conteudo = conteudo;
+           
+        }
+    }
 }
+
