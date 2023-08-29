75afb708063926133be13ff5576dcc3878630542
DANIELE APARECIDA DOS SANTOS <daniele.asantos32@senacsp.edu.br>
2023-08-29 17:01:30 -0300

    classess

--- Pi-Serasa-Starlents/Properties/postagens.cs
+++ Pi-Serasa-Starlents/Properties/postagens.cs
@@ -1,8 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
-public class postagens
+
+namespace Pi-Serasa-Starlents
 {
-	public postagens()
-	{
-	}
+    internal class postagens
+    {
+
+
+      public int id;
+      public string id usuario;
+	  public string postagem;
+      public string registro;
+
+
+
+      public postagens()
+      {
+
+
+      }
+
+      public postagens(int id, string id_usuario, string postagem, string registro)
+      {
+        this.id = id;
+        this.id_usuario = id_usuario;
+        this.postagerm = postagem;
+        this.registro = registro;
+      }
+
+      public List<postagens>buscaTodos() 
+      {
+        string query = "SELECT * FROM postagen;";
+
+        DataTable tabela = Conexao.executaQuery(query);
+
+      }
+
+    }
+
+
 }
+
--- Pi-Serasa-Starlents/avatar.cs
+++ Pi-Serasa-Starlents/avatar.cs
@@ -1,9 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class avatar
 
+namespace Pi_Serasa_Starlents
 {
-	public avatar()
-	{
-	}
+    internal class avatar
+    {
+        public int id;
+        public string id_usuario;
+        public string rosto;
+        public string olho;
+        public string pele;
+        public string cabelo;
+
+
+        public avatar(int id, string id_usuario, string rosto, string olho, string pele, string cabelo;) 
+        { 
+            this.id = id;
+            this.id_usuario = id_usuario;
+            this.roso = rosto;
+            this.olho = olho;
+            this.pele = pele;
+            this.cabelo = cabelo;
+            
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
