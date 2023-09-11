84cf4f2c40967655bb083d69a84cd8ed69a4da33
biancaScanfellaa <143142897+biancaScanfellaa@users.noreply.github.com>
2023-09-11 17:23:39 -0300

    tela de perfil nova

--- Pi-Serasa-Starlents/Perfil.cs
+++ Pi-Serasa-Starlents/Perfil.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WiLBiT;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace Pi_Serasa_Starlents
@@ -20,6 +21,9 @@ namespace Pi_Serasa_Starlents
             InitializeComponent();
         }
 
+        Interesses i = new Interesses();
+       
+
         public void carregaform(Form form)
         {
             form.TopLevel = false;
@@ -28,9 +32,6 @@ namespace Pi_Serasa_Starlents
             wilBitGradientPanel1.Controls.Add(form);
             form.Show();
         }
-
-
-
         void rendeirizaInterface()
         {
             WindowState = FormWindowState.Maximized;
@@ -40,20 +41,6 @@ namespace Pi_Serasa_Starlents
 
             wilBitGradientPanel1.Location = new Point(0, 0);
             wilBitGradientPanel1.Size = new Size(ClientSize.Width, ClientSize.Height);
-
-            // panel1.Size = new Size(ClientSize.Width, 63);
-            // panelIconesPer.Location = new Point(ClientSize.Width / 2 - panelIconesPer.Width / 2);
-
-            //label3.Location = new Point(ClientSize.Height / 2 - label3.Width / 2);
-            //  label3.Location = new Point(ClientSize.Width / 2 - label3.Size.Width / 2, ClientSize.Height / 2 - label3.Size.Height / 2 - 300);
-
-            //label1.Location = new Point(ClientSize.Height / 2 - label3.Width / 2);
-            //label1.Location = new Point(ClientSize.Width / 2 - label3.Size.Width / 2, ClientSize.Height / 2 - label3.Size.Height / 2 - 100);
-
-            // panelDescri.Location = new Point(ClientSize.Height - 100);
-            // panelDescri.Location = new Point(ClientSize.Width / 2 - panelDescri.Size.Width / 2 -50 , ClientSize.Height / 2 - panelDescri.Size.Height / 2 - 30);
-
-
         }
 
         private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
@@ -63,8 +50,6 @@ namespace Pi_Serasa_Starlents
 
         private void pictureBox6_Click(object sender, EventArgs e)
         {
-            // botao notificacao
-            carregaform(new Notificacao());
         }
 
         private void panel3_Paint(object sender, PaintEventArgs e)
@@ -76,9 +61,9 @@ namespace Pi_Serasa_Starlents
         {
             rendeirizaInterface();
 
+            lblNome.Text = Program.usuario.nome;
+
             Usuario usuario = new Usuario();
-
-
             List<Usuario> usuarios = usuario.ListarUsuarios();
 
             foreach (Usuario u in usuarios)
@@ -87,47 +72,43 @@ namespace Pi_Serasa_Starlents
                 wilBitTextBox6.Text = (u.descricao).ToString();
                 pictureBox1.ImageLocation = (u.avatar).ToString();
             }
-        }
 
-        private void pictureBox3_Click(object sender, EventArgs e)
-        {
-            // botao do perfil
-            carregaform(new Perfil());
+            usuarios = usuario.buscaTodosPorIdUsuario(Program.usuario.id);
+            foreach (Usuario u in usuarios)
+            {
+                lblNome.Text = (u.nome).ToString();
+                wilBitTextBox6.Text = (u.descricao).ToString();
+                pictureBox1.ImageLocation = (u.avatar).ToString();
+            }
         }
-
         private void label4_Click(object sender, EventArgs e)
         {
-
         }
-
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            // botao da tela inicial
-            carregaform(new TelaDeInicio());
-
         }
-
-        private void pictureBox9_Click(object sender, EventArgs e)
-        {
-            // botao das mensagens
-            carregaform(new TelaDeMensagens());
-        }
-
         private void pictureBox8_Click(object sender, EventArgs e)
         {
-            // botao do configuração
-
-
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
-
+            // label descricao
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            //premium
+        }
 
+        private void lblNome_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void wilBitTextBox6__TextChanged(object sender, EventArgs e)
+        {
+
+            // descricao
         }
     }
 }
